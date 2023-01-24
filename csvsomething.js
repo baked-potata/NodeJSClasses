fd6a650b1e2533ccbc72153f91dc3f1a5492abbc
rahul <baked-potata@protonmail.com>
2023-01-24 11:55:30 +0530

    something something csv with parse

--- csvsomething.js
+++ csvsomething.js
@@ -1,5 +1,27 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
-const data = [];
+const records = [];
+const isPrat = (data) => data["last_name"] === "Centini";
 
-fs.createReadStream("./100-contacts.csv");
+fs.createReadStream("./100-contacts.csv")
+  .pipe(
+    parse({
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    // console.log(data);
+    // records.push(data);
+    if (isPrat(data)) {
+      records.push(data);
+      console.log(data);
+    }
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    // console.log(`${records.length} : ${JSON.stringify(records)} `);
+  });
+
+// console.log(records);
